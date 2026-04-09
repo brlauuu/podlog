59ab3112a6c7af63a5f2edc7f2943859bff5e33b
Đorđe Relić <dorde.relic@proton.me>
2026-04-09 21:15:12 +0200

    test: use full web app coverage denominator (#281)

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -5,6 +5,12 @@ const createJestConfig = nextJest({ dir: "./" });
 const config = createJestConfig({
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/__tests__/**",
+    "!src/**/__mocks__/**",
+  ],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
